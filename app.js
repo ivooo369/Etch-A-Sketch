5cbd8e5f30a16f095bdd366833cb64fb0feee620
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2022-08-27 14:47:33 +0300

    Create a webpage with a 16x16 grid of square divs

---
+++ app.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body");
+const container = document.createElement("div");
+container.setAttribute("id", "container");
+body.appendChild(container);
+
+function makeRows(rows, columns) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", columns);
+  for (i = 0; i < rows * columns; i++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  }
+}
+
+makeRows(16, 16);
