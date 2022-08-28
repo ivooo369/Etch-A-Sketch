5aa478d709f80b7bfa518f4a2de08a5a959a3aa7
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2022-08-28 21:30:13 +0300

    Add an option to create a new grid with dimensions given by the user

--- app.js
+++ app.js
@@ -2,21 +2,46 @@ const body = document.querySelector("body");
 const container = document.createElement("div");
 container.setAttribute("id", "container");
 body.appendChild(container);
+const button = document.createElement("button");
+button.style.marginBottom = "20px";
+button.innerText = "New Grid";
+container.appendChild(button);
+const grid = document.createElement("div");
+grid.classList.add("grid");
+container.appendChild(grid);
 
-function makeRows(rows, columns) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", columns);
+function makeGrid(rows, columns) {
+  grid.style.setProperty("--grid-rows", rows);
+  grid.style.setProperty("--grid-cols", columns);
   for (i = 0; i < rows * columns; i++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    grid.appendChild(cell).className = "grid-item";
     fillCell(cell);
   }
 }
-
-makeRows(16, 16);
 
 function fillCell(cell) {
   cell.addEventListener("mouseover", () => {
     cell.style.backgroundColor = "black";
   });
 }
+
+makeGrid(16, 16);
+
+function createNewGrid() {
+  button.addEventListener("click", () => {
+    document.querySelectorAll(".grid-item").forEach((e) => e.remove());
+    let userInput = prompt("Enter a number");
+
+    if (userInput > 100) {
+      alert("Oops, It seems that the number is too big! Please, try again!");
+      return;
+    }
+
+    rows = userInput;
+    columns = userInput;
+    makeGrid(rows, columns);
+  });
+}
+
+createNewGrid();
