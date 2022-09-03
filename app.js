475925aba7f69c4a50346c2e82b69e0e5013d432
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2022-09-03 17:35:11 +0300

    Make some styling changes and add options to chose custom color for the pen and custom color for the grid background

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const body = document.querySelector("body");
+const cell = document.querySelector(".grid-item");
 const grid = document.querySelector(".grid");
+const penCustomColor = document.querySelector("#input-pen-color");
+const backgroundCustomColor = document.querySelector("#input-background-color");
 
 let isMouseDown = false;
 document.body.onmousedown = () => (isMouseDown = true);
 document.body.onmouseup = () => (isMouseDown = false);
-
-let isMouseHover = false;
-document.body.onmouseenter = () => (isMouseHover = true);
-document.body.onmouseleave = () => (isMouseHover = false);
 
 function makeGrid(rows, columns) {
   grid.style.setProperty("--grid-rows", rows);
@@ -14,38 +14,87 @@ function makeGrid(rows, columns) {
   for (i = 0; i < rows * columns; i++) {
     let cell = document.createElement("div");
     grid.appendChild(cell).className = "grid-item";
+    chosePenColor(cell);
+    choseBackgroundColor();
     clickButtonBlack(cell);
     clickButtonRainbow(cell);
     clickButtonEraser(cell);
     clickButtonClear();
   }
 }
-clickGridSizeButtons();
+
 makeGrid(15, 15);
 
+function setPenColor(cell) {
+  cell.addEventListener("mouseenter", () => {
+    if (isMouseDown) {
+      cell.style.backgroundColor = penCustomColor.value;
+    }
+  });
+}
+
+function chosePenColor(cell) {
+  penCustomColor.addEventListener("input", function () {
+    setPenColor(cell);
+  });
+}
+
+function setBackgroundColor() {
+  body.addEventListener("click", () => {
+    grid.style.backgroundColor = backgroundCustomColor.value;
+  });
+}
+
+function choseBackgroundColor() {
+  backgroundCustomColor.addEventListener("input", function () {
+    setBackgroundColor();
+  });
+}
+
 function fillCellBlack(cell) {
-  cell.addEventListener("mousemove", () => {
+  cell.addEventListener("mouseenter", () => {
     if (isMouseDown) {
       cell.style.backgroundColor = "black";
     }
   });
 }
 
+function clickButtonBlack(cell) {
+  const buttonBlack = document.querySelector("#btn-black");
+  buttonBlack.addEventListener("click", () => {
+    fillCellBlack(cell);
+  });
+}
+
 function fillCellRainbow(cell) {
-  const rainbowColors = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"];
-  const randomColor = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
-  cell.addEventListener("mousemove", () => {
+  cell.addEventListener("mouseenter", () => {
+    const rainbowColors = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"];
+    const randomColor = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
     if (isMouseDown) {
-      cell.style.backgroundColor = `${randomColor}`;
+      cell.style.backgroundColor = randomColor;
     }
   });
 }
 
+function clickButtonRainbow(cell) {
+  const buttonRainbow = document.querySelector("#btn-rainbow");
+  buttonRainbow.addEventListener("click", () => {
+    fillCellRainbow(cell);
+  });
+}
+
 function erase(cell) {
-  cell.addEventListener("mousemove", () => {
+  cell.addEventListener("mouseenter", () => {
     if (isMouseDown) {
-      cell.style.backgroundColor = "white";
+      cell.style.backgroundColor = "";
     }
+  });
+}
+
+function clickButtonEraser(cell) {
+  const buttonEraser = document.querySelector("#btn-eraser");
+  buttonEraser.addEventListener("click", () => {
+    erase(cell);
   });
 }
 
@@ -55,79 +104,28 @@ function clear() {
   });
 }
 
-function clickButtonBlack(cell) {
-  const buttonBlack = document.querySelector(".btn-black");
-  buttonBlack.addEventListener("click", () => {
-    fillCellBlack(cell);
-  });
-}
-
-function clickButtonRainbow(cell) {
-  const buttonRainbow = document.querySelector(".btn-rainbow");
-  buttonRainbow.addEventListener("click", () => {
-    fillCellRainbow(cell);
-  });
-}
-
-function clickButtonEraser(cell) {
-  const buttonEraser = document.querySelector(".btn-eraser");
-  buttonEraser.addEventListener("click", () => {
-    erase(cell);
-  });
-}
-
 function clickButtonClear() {
-  const buttonClear = document.querySelector(".btn-clear");
+  const buttonClear = document.querySelector("#btn-clear");
   buttonClear.addEventListener("click", () => {
     clear();
   });
 }
 
-function clickGridSizeButtons() {
-  const button10x10 = document.querySelector("#btn-10x10");
-  button10x10.addEventListener("click", function () {
-    document.querySelectorAll(".grid-item").forEach((e) => e.remove());
-    makeGrid(10, 10);
-  });
-
-  const button15x15 = document.querySelector("#btn-15x15");
-  button15x15.addEventListener("click", function () {
-    document.querySelectorAll(".grid-item").forEach((e) => e.remove());
-    makeGrid(15, 15);
-  });
-
-  const button20x20 = document.querySelector("#btn-20x20");
-  button20x20.addEventListener("click", function () {
-    document.querySelectorAll(".grid-item").forEach((e) => e.remove());
-    makeGrid(20, 20);
-  });
-
-  const button25x25 = document.querySelector("#btn-25x25");
-  button25x25.addEventListener("click", function () {
-    document.querySelectorAll(".grid-item").forEach((e) => e.remove());
-    makeGrid(25, 25);
-  });
-
-  const button30x30 = document.querySelector("#btn-30x30");
-  button30x30.addEventListener("click", function () {
-    document.querySelectorAll(".grid-item").forEach((e) => e.remove());
-    makeGrid(30, 30);
-  });
-}
-
 function createNewGrid() {
-  const buttonNewGrid = document.querySelector(".btn-new-grid");
+  let inputValue = document.querySelector("#grid-size");
+  inputValue.addEventListener("input", () => {
+    rows = inputValue.value;
+    columns = inputValue.value;
+  });
+
+  const buttonNewGrid = document.querySelector(".btn-set-grid-size");
   buttonNewGrid.addEventListener("click", () => {
     document.querySelectorAll(".grid-item").forEach((e) => e.remove());
-    let userInput = prompt("Enter a number");
-
-    if (userInput > 100) {
-      alert("Oops, It seems that the number is too big! Please, try again!");
-      return;
+    if (inputValue.value <= 50) {
+      makeGrid(rows, columns);
+    } else {
+      alert("OOOPss");
     }
-    rows = userInput;
-    columns = userInput;
-    makeGrid(rows, columns);
   });
 }
 
